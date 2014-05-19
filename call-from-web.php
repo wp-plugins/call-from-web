25418a796a6338deac253347adfae397fd4b33cb
call_from_web <call_from_web@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-19 22:23:31 +0000

    Update plugin settings namespace.
    git-svn-id: https://plugins.svn.wordpress.org/call-from-web/trunk@917492 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- call-from-web.php
+++ call-from-web.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: Call From Web
  * Plugin URI: http://www.call-from-web.com/plugins/wordpress
- * Description: Let your visitors contact you by phone.
+ * Description: Let your visitors call you by phone for free.
  * Version: 2.3
  * Author: Call From Web
  * Author URI: http://www.call-from-web.com
@@ -24,7 +24,7 @@ function cfw_options_page_fn() {
     <h2>Call From Web</h2>
     Set up your domain widget ID here to be able to view your comments. <h3>Need a valid widget ID? <a href="http://www.call-from-web.com/users/sign_up">Sign up</a>.</h3>
     <form action="options.php" method="post">
-    <?php settings_fields('plugin_options'); ?>
+    <?php settings_fields('cfw_plugin_options'); ?>
     <?php do_settings_sections(__FILE__); ?>
     <p class="submit">
       <input name="Submit" type="submit" class="button-primary" value="<?php esc_attr_e('Save Changes'); ?>" />
@@ -38,28 +38,28 @@ function cfw_options_page_fn() {
 add_action('admin_init', 'cfw_options_init_fn' );
 // Register our settings. Add the settings section, and settings fields
 function cfw_options_init_fn(){
-  register_setting('plugin_options', 'plugin_options', 'cfw_plugin_options_validate' );
-  add_settings_section('main_section', 'Button Settings', 'cfw_section_text_fn', __FILE__);
-  add_settings_field('cfw_profile', 'Widget ID', 'cfw_setting_profile', __FILE__, 'main_section');
-  add_settings_field('cfw_button_title', 'Button Title', 'cfw_setting_button_title', __FILE__, 'main_section');
+  register_setting('cfw_plugin_options', 'cfw_plugin_options', 'cfw_plugin_options_validate' );
+  add_settings_section('cfw_main_section', 'Button Settings', 'cfw_section_text_fn', __FILE__);
+  add_settings_field('cfw_profile', 'Widget ID', 'cfw_setting_profile', __FILE__, 'cfw_main_section');
+  add_settings_field('cfw_button_title', 'Button Title', 'cfw_setting_button_title', __FILE__, 'cfw_main_section');
 }
 
 function cfw_section_text_fn(){}
 
 function cfw_setting_profile() {
-  $options = get_option('plugin_options');
-  echo "<input id='plugin_text_string' name='plugin_options[cfw_profile]' size='40' type='text' value='{$options['cfw_profile']}' />";
+  $options = get_option('cfw_plugin_options');
+  echo "<input id='plugin_text_string' name='cfw_plugin_options[cfw_profile]' size='40' type='text' value='{$options['cfw_profile']}' />";
 }
 
 function cfw_setting_key() {
-  $options = get_option('plugin_options');
-  echo "<input id='plugin_text_string' name='plugin_options[cfw_key]' size='40' type='text' value='{$options['cfw_key']}' />";
+  $options = get_option('cfw_plugin_options');
+  echo "<input id='plugin_text_string' name='cfw_plugin_options[cfw_key]' size='40' type='text' value='{$options['cfw_key']}' />";
 }
 
 function cfw_setting_button_title() {
-  $options = get_option('plugin_options');
+  $options = get_option('cfw_plugin_options');
   $button_title = empty($options['cfw_button_title']) ? 'Contact Us' : $options['cfw_button_title'];
-  echo "<input id='plugin_text_string' name='plugin_options[cfw_button_title]' size='40' type='text' value='{$button_title}' />";
+  echo "<input id='plugin_text_string' name='cfw_plugin_options[cfw_button_title]' size='40' type='text' value='{$button_title}' />";
 }
 
 
@@ -73,7 +73,7 @@ function cfw_plugin_options_validate($input) {
 // add meta info
 add_action('wp_footer', 'cfw_addButton');
 function cfw_addButton(){
-  $o = get_option('plugin_options');
+  $o = get_option('cfw_plugin_options');
   $profile = empty($o['cfw_profile']) ? "" : "/{$o['cfw_profile']}";
   $button_title = empty($o['cfw_button_title']) ? 'Contact Us' : $o['cfw_button_title'];
   echo "<a href=\"http://www.call-from-web.com/request{$profile}\" class=\"call-from-web\">{$button_title}</a>";
@@ -92,6 +92,6 @@ EOS;
     <?php
 }
 
-$o = get_option('plugin_options');
+$o = get_option('cfw_plugin_options');
 if (empty($o['cfw_profile'])) { add_action( 'admin_notices', 'cfw_no_api_key_provided_admin_notice'); }
 
